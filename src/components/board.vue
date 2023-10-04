c227db205a127f6cd355ba93d47656692654cf32
Drew Wyatt <drew.wyatt@classdojo.com>
2023-10-04 01:54:40 +0000

    use constants in css

--- src/components/board.vue
+++ src/components/board.vue
@@ -20,11 +20,14 @@ import Space from '~/components/space.vue'
 }
 
 .spaces {
+  --num-colums: v-bind('BOARD_WIDTH');
+  --num-rows: v-bind('BOARD_HEIGHT');
+
   border: none;
   display: grid;
   grid-auto-flow: column;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(6, 1fr);
+  grid-template-columns: repeat(var(--num-colums), 1fr);
+  grid-template-rows: repeat(var(--num-rows), 1fr);
   row-gap: 10px;
 }
 </style>
